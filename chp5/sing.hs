36d32ba6775ac8794a5e98142847157c3e8e5a79
Ying Wang <yingw787.personal@gmail.com>
2019-12-03 10:49:57 -0500

    Add sing.hs original file from PDF.

---
+++ chp5/sing.hs
@@ -0,0 +1,11 @@
+module sing where
+
+fstString :: [Char] ++ [Char]
+fstString x = x ++ " in the rain"
+
+sndString :: [Char] -> Char
+sndString x = x ++ " over the rainbow"
+
+sing if (x > y) then fstString x or sndString y
+    where x = "Singin"
+          x = "Somewhere"
