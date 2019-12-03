e75c44a022e401ea30ae2bd9b87246d6ecd5fc8c
Ying Wang <yingw787.personal@gmail.com>
2019-12-03 18:32:59 -0500

    Update dates.hs.

--- chp6/dates.hs
+++ chp6/dates.hs
@@ -7,9 +7,11 @@ data DayOfWeek =
     -- derive from `Show` in order to be able to execute print methods, called
     -- naturally by GHCi.
     --
-    -- derive from `Ord` to be able to set a comparison between days of the
-    -- week.
-    deriving (Ord, Show)
+    -- derive from `Ord` to be able to set a custom comparison between days of
+    -- the week. If a custom comparison is not implemented, then comparison will
+    -- set values to the left of the instantiation to be less than the values on
+    -- the right.
+    deriving (Show)
 
 -- Define the day of the week and numerical day of the month.
 data Date =
@@ -26,6 +28,18 @@ instance Eq DayOfWeek where
     (==) Sun Sun = True
     -- Unconditional case, for checking mismatched cases.
     (==) _ _ = False
+
+-- Implement a custom comparison where Friday is always the greatest day of the
+-- week.
+--
+-- Note that this comparison does not specify equality for same dates besides
+-- Friday. However, since Ord inherits Eq, the underlying Eq comparison takes
+-- over and operations like `Mon == Mon` are still True.
+instance Ord DayOfWeek where
+    compare Fri Fri = EQ
+    compare Fri _ = GT
+    compare _ Fri = LT
+    compare _ _ = EQ
 
 -- Implement equality for data type 'Date'.
 instance Eq Date where
