ddb021aca12f22f26131329dbe416f609539a712
Ying Wang <yingw787.personal@gmail.com>
2019-12-03 18:05:05 -0500

    Update dates.hs add derivation from Ord/Show.

--- chp6/dates.hs
+++ chp6/dates.hs
@@ -4,6 +4,12 @@ module Dates where
 -- Define the day of the week.
 data DayOfWeek =
     Mon | Tue | Weds | Thu | Fri | Sat | Sun
+    -- derive from `Show` in order to be able to execute print methods, called
+    -- naturally by GHCi.
+    --
+    -- derive from `Ord` to be able to set a comparison between days of the
+    -- week.
+    deriving (Ord, Show)
 
 -- Define the day of the week and numerical day of the month.
 data Date =
