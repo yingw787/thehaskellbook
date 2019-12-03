9aac68b0fff5777988c3e392c1d67fd4b1bbbe65
Ying Wang <yingw787.personal@gmail.com>
2019-12-03 12:40:22 -0500

    Add dates.hs indentation errors implementing equality checks in ghci.

---
+++ chp6/dates.hs
@@ -0,0 +1,26 @@
+-- dates.hs
+module Dates where
+
+-- Define the day of the week.
+data DayOfWeek =
+    Mon | Tue | Weds | Thu | Fri | Sat | Sun
+
+-- Define the day of the week and numerical day of the month.
+data Date =
+    Date DayOfWeek Int
+
+-- Implement equality for data type 'DayOfWeek'.
+instance Eq DayOfWeek where
+    (==) Mon Mon = True
+    (==) Tue Tue = True
+    (==) Weds Weds = True
+    (==) Thu Thu = True
+    (==) Fri Fri = True
+    (==) Sat Sat = True
+    (==) Sun Sun = True
+    (==) _ _ = False
+
+-- Implement equality for data type 'Date'.
+instance Eq Date where
+    (==) (Date weekday dayOfMonth) (Date weekday' dayOfMonth') =
+        weekday == weekday' && dayOfMonth == dayOfMonth'
