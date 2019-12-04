41ad7e91a15ef71f9a5709d6e403604cfb82d90d
Ying Wang <yingw787.personal@gmail.com>
2019-12-04 12:27:10 -0500

    Update MatchTheTypes.hs.

--- chp6/MatchTheTypes.hs
+++ chp6/MatchTheTypes.hs
@@ -2,6 +2,8 @@
 -- MatchTheTypes.hs
 module MatchTheTypes where
 
+-- import Data.List
+
 -- (1)
 -- i :: Num a => a
 -- i = 1
@@ -43,3 +45,46 @@ module MatchTheTypes where
 --
 -- freud' :: Int -> Int
 -- freud' x = x
+
+-- (7)
+-- myX = 1 :: Int
+-- sigmund :: Int -> Int
+-- sigmund x = myX
+--
+-- myX = 1 :: Int
+-- sigmund :: a -> a
+-- sigmund x = myX
+
+-- (8)
+-- myX = 1 :: Int
+-- sigmund' :: Int -> Int
+-- sigmund' x = myX
+--
+-- myX = 1 :: Int
+-- sigmund' :: Num a => a -> a
+-- sigmund' x = myX
+
+-- (9)
+-- jung :: Ord a => [a] -> a
+-- jung xs = head (sort xs)
+--
+-- jung :: [Int] -> Int
+-- jung xs = head (sort xs)
+
+-- (10)
+-- young :: [Char] -> Char
+-- young xs = head (sort xs)
+--
+-- young :: Ord a => [a] -> a
+-- young xs = head (sort xs)
+
+-- (11)
+-- mySort :: [Char] -> [Char]
+-- mySort = sort
+-- signifier :: [Char] -> Char
+-- signifier xs = head (mySort xs)
+--
+-- mySort :: [Char] -> [Char]
+-- mySort = sort
+-- signifier :: Ord a => [a] -> a
+-- signifier xs = head (mySort xs)
