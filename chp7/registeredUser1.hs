2bdf08314c7a477bb8bc758dd1deb16b595f17f9
Ying Wang <yingw787.personal@gmail.com>
2019-12-08 14:00:26 -0500

    Update registeredUser1.hs.

--- chp7/registeredUser1.hs
+++ chp7/registeredUser1.hs
@@ -7,6 +7,9 @@ newtype Username =
 newtype AccountNumber =
     AccountNumber Integer
 
+-- Sum of two constructors, `UnregisteredUser` and `RegisteredUser`. Pattern
+-- matching enables dispatching based on input arguments. If `RegisteredUser`,
+-- then it is a product of two `newtypes`s, `Username` and `AccountNumber`.
 data User =
     UnregisteredUser
     | RegisteredUser Username AccountNumber
