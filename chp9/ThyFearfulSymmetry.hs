29a38e3917eef6cde8ee78962509bccaa5a214d0
Ying Wang <yingw787.personal@gmail.com>
2019-12-10 15:29:47 -0500

    Update ThyFearfulSymmetry.hs.

--- chp9/ThyFearfulSymmetry.hs
+++ chp9/ThyFearfulSymmetry.hs
@@ -9,10 +9,24 @@ myWords someString = go someString []
     where
         go currentString currentListOfStrings
             | currentString == "" = reverse currentListOfStrings
-            | currentString == " " = reverse currentListOfStrings
+            | (take 1 currentString) == " " = go
+                (dropWhile (== ' ') currentString)
+                (currentListOfStrings)
             | otherwise = go
                 (dropWhile (== ' ') $ dropWhile (/= ' ') currentString)
                 (takeWhile (/= ' ') currentString : currentListOfStrings)
+
+-- ANSWER KEY: https://github.com/johnchandlerburnham/hpfp
+--
+-- (PERSONAL NOTE: This works for the simple solution, but does not effectively
+-- trim spaces from the beginning or the end of the string.)
+--
+-- split :: String -> [String]
+-- split [] = []
+-- split x = word : split rest
+--     where
+--         word = takeWhile (/= ' ') x
+--         rest = (drop 1) $ dropWhile (/= ' ') x
 
 -- (2)
 
