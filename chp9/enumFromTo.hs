849fe7c110c4654233c50417694b5ea0a6682b2d
Ying Wang <yingw787.personal@gmail.com>
2019-12-10 11:57:38 -0500

    Add enumFromTo.hs.

---
+++ chp9/enumFromTo.hs
@@ -0,0 +1,14 @@
+-- enumFromTo.hs
+module EnumFromTo where
+
+eftBool :: Bool -> Bool -> [Bool]
+eftBool = undefined
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd = undefined
+
+eftInt :: Int -> Int -> [Int]
+eftInt = undefined
+
+eftChar :: Char -> Char -> [Char]
+eftChar = undefined
