6374f9451a5627db2ba9aa1002c0ceae4e185abc
Ying Wang <yingw787.personal@gmail.com>
2019-12-10 13:45:12 -0500

    Update enumFromTo.hs.

--- chp9/enumFromTo.hs
+++ chp9/enumFromTo.hs
@@ -45,3 +45,51 @@ eftChar x y = go x y []
             | first > second = currentList
             | first == second = reverse (first : currentList)
             | otherwise = go (succ first) second (first : currentList)
+
+-- Generalizing based on ANSWER KEY: https://github.com/johnchandlerburnham/hpfp
+
+-- Typeclass constraints that type must be order-able, and enumerable.
+eft :: (Ord a, Enum a) => a -> a -> [a]
+eft x y = go x y []
+    where
+        go a b c
+            | a > b = c
+            -- | a == b = reverse (a : c)
+            --
+            -- The operand `reverse` is weird, this is replacement.
+            | a == b = c ++ a:[]
+            | otherwise = go (succ a) b (a : c)
+
+-- Use recursion and avoid having to use `go` functions.
+--
+-- Method `eft3`
+eft3 :: (Ord a, Enum a) => a -> a -> [a]
+eft3 x y
+    | x > y = []
+    | x == y = [x]
+    -- The recursion works because the ending constructor is of type list, which
+    -- successfully applies the `cons` constructor.
+    -- 1 : []
+    -- [1]
+    -- 1 : 2 : []
+    -- [1,2]
+    -- 1 : 2 : [3]
+    -- [1,2,3]
+    | otherwise = x : eft3 (succ x) y
+
+-- We can get rid of the `Ord` typeclass constraint by leveraging the mapping
+-- between an Enum and Int using method `fromEnum`:
+--
+-- fromEnum True
+-- 1
+-- fromEnum False
+-- 0
+-- fromEnum 'a'
+-- 97 -- Unicode decimal representation
+-- fromEnum 'Ļ'
+-- 315 -- Yes, it really is Unicode
+eft4 :: Enum a => a -> a -> [a]
+eft4 x y
+    | fromEnum x > fromEnum y = []
+    | fromEnum x == fromEnum y = [x]
+    | otherwise = x : eft4 (succ x) y
