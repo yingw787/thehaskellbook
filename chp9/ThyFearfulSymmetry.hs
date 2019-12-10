6a3dac0fe460e3f8b0b357392bfc9ea52872ca29
Ying Wang <yingw787.personal@gmail.com>
2019-12-10 15:39:04 -0500

    Update ThyFearfulSymmetry.hs.

--- chp9/ThyFearfulSymmetry.hs
+++ chp9/ThyFearfulSymmetry.hs
@@ -29,5 +29,34 @@ myWords someString = go someString []
 --         rest = (drop 1) $ dropWhile (/= ' ') x
 
 -- (2)
+firstSentence = "Tyger Tyger, burning bright\n"
+secondSentence = "In the forests of the night\n"
+thirdSentence = "What immortal hand or eye\n"
+fourthSentence = "Could frame thy fearful symmetry?"
+sentences = firstSentence ++ secondSentence ++ thirdSentence ++ fourthSentence
+
+shouldEqual =
+-- I think inline (=) is special for some reason; placing left bracket on
+-- newline doesn't result in compile-time error, whereas placing it inline to
+-- `(=)` does result in compile-time error.
+    [
+        "Tyger Tyger, burning bright",
+        "In the forests of the night",
+        "What immortal hand or eye",
+        "Could frame thy fearful symmetry?"
+    ]
+
+myLines :: String -> [String]
+myLines [] = []
+myLines x = word : myLines rest
+    where
+        word = takeWhile (/= '\n') x
+        rest = (drop 1) $ dropWhile (/= '\n') x
+
+main :: IO ()
+main =
+    print $
+    "Are they equal? "
+    ++ show (myLines sentences == shouldEqual)
 
 -- (3)
