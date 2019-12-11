228d9fbfe06c48256953deb24296bca6a1ea7651
Ying Wang <yingw787.personal@gmail.com>
2019-12-11 13:51:34 -0500

    Update StandardFunctions.hs.

--- chp9/StandardFunctions.hs
+++ chp9/StandardFunctions.hs
@@ -12,4 +12,21 @@ myAnd (x : xs) =
 -- direct recursion, using (&&)
 myAnd' :: [Bool] -> Bool
 myAnd' [] = True
-myAnd (x : xs) = x && myAnd xs
+myAnd' (x : xs) = x && myAnd xs
+
+-- 1)
+myOr :: [Bool] -> Bool
+-- If there are no matches, then return False by default.
+myOr [] = False
+myOr (x : xs) =
+    if x == True
+    then True
+    else myOr xs
+
+myOr' :: [Bool] -> Bool
+myOr' [] = False
+myOr' (x : xs) = x || myOr' xs
+
+-- 2)
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f a = myOr [f x | x <- a]
