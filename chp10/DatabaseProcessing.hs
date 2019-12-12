56af5b0cfe8f5e397ba0a74a3e9d78fbcd43ec44
Ying Wang <yingw787.personal@gmail.com>
2019-12-12 14:43:27 -0500

    Update DatabaseProcessing.hs.

--- chp10/DatabaseProcessing.hs
+++ chp10/DatabaseProcessing.hs
@@ -21,8 +21,21 @@ theDatabase =
     ]
 
 -- 1)
+--
+-- (LITERALLY NO IDEA HOW TO GET STARTED, BLANKING OUT)
+--
+-- (ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+--
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate a = undefined
+filterDbDate = map unpack . filter isADbDate where
+    isADbDate (DbDate _) = True
+    isADbDate _ = False
+    unpack (DbDate utc) = utc
+
+filterDbDate' :: [DatabaseItem] -> [UTCTime]
+filterDbDate' = foldr unpackDate [] where
+    unpackDate (DbDate utc) acc = utc : acc
+    unpackDate _ acc = acc
 
 -- 2)
 filterDbNumber :: [DatabaseItem] -> [Integer]
@@ -39,3 +52,9 @@ sumDb = undefined
 -- 5)
 avgDb :: [DatabaseItem] -> Double
 avgDb = undefined
+
+main :: IO ()
+main = do
+    -- 1)
+    print $ filterDbDate theDatabase
+    print $ filterDbDate' theDatabase
