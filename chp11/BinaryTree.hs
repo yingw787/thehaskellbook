0d30359917a845882cb682ed557831cf048936d3
Ying Wang <yingw787.personal@gmail.com>
2019-12-16 17:34:50 -0500

    Update BinaryTree.hs.

--- chp11/BinaryTree.hs
+++ chp11/BinaryTree.hs
@@ -19,6 +19,8 @@ insert' b (Node left a right)
 mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
 mapTree _ Leaf = Leaf
 -- SOLUTION BELOW
+--
+-- (CORRECT BY GHCI OUTPUT)
 mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
 
 testTree' :: BinaryTree Integer
@@ -34,6 +36,61 @@ mapOkay =
     then print "yup okay!"
     else error "test failed!"
 
+-- main :: IO ()
+-- main = do
+--     mapOkay
+
+-- Convert binary trees to lists
+preorder :: BinaryTree a -> [a]
+-- (INCORRECT, COMPILE-TIME ERROR)
+-- preorder _ Leaf = []
+-- preorder (Node Leaf a Leaf) = a
+-- preorder (Node left a right) = Node (preorder left) a right
+--
+-- (PERSONAL NOTE: not sure where I was going with this... `cons` operator
+-- concatenating a binary tree into a list??
+--
+-- preorder (Node Leaf a Leaf) = a
+-- preorder (Node left a right) = (preorder (BinaryTree left)) : B
+--
+-- (SIGH ANSWER KEY..)
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ (preorder left) ++ (preorder right)
+
+-- (CORRECT BY GHCI OUTPUT)
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = (preorder left) ++ [a] ++ (preorder right)
+
+-- (CORRECT BY GHCI OUTPUT)
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = (postorder left) ++ (postorder right) ++ [a]
+
+testTree :: BinaryTree Integer
+testTree =
+    Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+    if preorder testTree == [2, 1, 3]
+    then putStrLn "Preorder fine!"
+    else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+    if inorder testTree == [1, 2, 3]
+    then putStrLn "Inorder fine!"
+    else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder =
+    if postorder testTree == [1, 3, 2]
+    then putStrLn "Postorder fine!"
+    else putStrLn "postorder failed check"
+
 main :: IO ()
 main = do
-    mapOkay
+    testPreorder
+    testInorder
+    testPostorder
