b20bdd037cb4560157f855a5b92680af0987b8f6
Ying Wang <yingw787.personal@gmail.com>
2019-12-17 21:53:47 -0500

    Update src/Main.hs.

--- chp13/hangman/src/Main.hs
+++ chp13/hangman/src/Main.hs
@@ -1,5 +1,56 @@
+-- src/Main.hs
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.IO (BufferMode(NoBuffering), hSetBuffering, stdout)
+import System.Random (randomRIO)
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter gameLength aw)
+  where gameLength w =
+          let l = length (w :: String)
+          in l >= minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  -- (INCORRECT, COMPILE-TIME ERROR)
+  -- randomIndex <- randomRIO (0, (length allWords - 1))
+  --
+  -- (FROM ANSWER KEY)
+  randomIndex <- randomRIO (0, (length wl) - 1)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+data Puzzle =
+  Puzzle String [Maybe Char] [Char]
+--       1      2            3
+-- 1: Word we're trying to guess
+-- 2: Characters filled in so far
+-- 3: Letters guessed so far
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $
+    fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
