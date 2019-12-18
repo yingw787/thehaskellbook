15df1133de355345f5754049c706e56a240f4657
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 15:26:11 -0500

    Update QuickCheck.hs.

--- chp14/addition/QuickCheck.hs
+++ chp14/addition/QuickCheck.hs
@@ -9,3 +9,9 @@ main = hspec $ do
     describe "Addition" $ do
         it "x + 1 is always greater than x" $ do
             property $ \x -> x + 1 > (x :: Int)
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
