bb1828ff7a2158a9b65847039606e4f01f089109
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 12:23:28 -0500

    Update Addition.hs.

--- chp14/addition/Addition.hs
+++ chp14/addition/Addition.hs
@@ -1,5 +1,12 @@
 -- Addition.hs
 module Addition where
 
-sayHello :: IO ()
-sayHello = putStrLn "hello!"
+-- Compiler parser doesn't have a means of recognizing import statements after
+-- method declarations.
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+    describe "Addition" $ do
+        it "1 + 1 is greater than 1" $ do
+            (1 + 1) > 1 `shouldBe` True
