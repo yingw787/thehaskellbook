cbc88d02815ac06bebefbadde5d218377b0241de
Ying Wang <yingw787.personal@gmail.com>
2019-12-17 21:00:25 -0500

    Update exe/Main.hs adding prompt for terminal input.

--- chp13/hello/exe/Main.hs
+++ chp13/hello/exe/Main.hs
@@ -2,9 +2,12 @@ module Main where
 
 import DogsRule
 import Hello
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
   name <- getLine
   sayHello name
   dogs
