4713f86a46ace89f2cf4bf7225164f5ba8288383
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 12:27:12 -0500

    Update Addition.hs.

--- chp14/addition/Addition.hs
+++ chp14/addition/Addition.hs
@@ -10,3 +10,5 @@ main = hspec $ do
     describe "Addition" $ do
         it "1 + 1 is greater than 1" $ do
             (1 + 1) > 1 `shouldBe` True
+        it "2 + 2 is equal to 4" $ do
+            2 + 2 `shouldBe` 4
