111b0038fbb25f5fd166cb94ec15cbfa508eb680
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 14:30:04 -0500

    Add QuickCheck.hs.

---
+++ chp14/addition/QuickCheck.hs
@@ -0,0 +1,11 @@
+-- QuickCheck.hs
+module QuickCheck where
+
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+    describe "Addition" $ do
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
