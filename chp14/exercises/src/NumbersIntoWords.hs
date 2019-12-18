fb1832e41371c0e1c01eebe57199d2b0a2f2eced
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 16:37:47 -0500

    Update NumbersIntoWords.hs.

--- chp14/exercises/src/NumbersIntoWords.hs
+++ chp14/exercises/src/NumbersIntoWords.hs
@@ -1,6 +1,29 @@
 -- NumbersIntoWords.hs
 module NumbersIntoWords where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+-- From WordNumber.hs, pasted here because WordNumber is not part of the same
+-- scope I think
+digitToWord :: Int -> String
+digitToWord n
+    | n == 0 = "zero"
+    | n == 1 = "one"
+    | n == 2 = "two"
+    | n == 3 = "three"
+    | n == 4 = "four"
+    | n == 5 = "five"
+    | n == 6 = "six"
+    | n == 7 = "seven"
+    | n == 8 = "eight"
+    | n == 9 = "nine"
+    | otherwise = error "Expecting a digit between 0 and 9 inclusive."
+
+digits :: Int -> [Int]
+digits n = go n []
+    where   go  value list
+                | value < 10 = [value] ++ list
+                | otherwise = go
+                    (fst $ value `divMod` 10)
+                    ([snd $ value `divMod` 10] ++ list)
+
+wordNumber :: Int -> String
+wordNumber n = concat (intersperse "-" (map (digitToWord) (digits n)))
