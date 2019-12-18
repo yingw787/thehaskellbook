e0abaa22a65620a8961e94b920e81268004289a1
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 16:18:19 -0500

    Update src/Arbitrary.hs.

--- chp14/morse/src/Arbitrary.hs
+++ chp14/morse/src/Arbitrary.hs
@@ -33,3 +33,18 @@ instance Arbitrary a => Arbitrary (Identity a) where
 -- Generate a concrete type argument `Int` for testing with Identity data type.
 identityGenInt :: Gen (Identity Int)
 identityGenInt = identityGen
+
+data Pair a b =
+    Pair a b deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+    arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
