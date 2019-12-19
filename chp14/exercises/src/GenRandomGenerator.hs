30695994d71e79101cda9928f99f3d439ab5ff61
Ying Wang <yingw787.personal@gmail.com>
2019-12-19 16:00:46 -0500

    Update GenRandomGenerator.hs.

--- chp14/exercises/src/GenRandomGenerator.hs
+++ chp14/exercises/src/GenRandomGenerator.hs
@@ -1,5 +1,36 @@
 -- GenRandomGenerator.hs
 module GenRandomGenerator where
 
-sayHello :: IO ()
-sayHello = putStrLn "Hello!"
+import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
+
+data Fool = Fulse | Frue deriving (Eq, Show)
+
+-- 1) Equal probabilities for each.
+--
+-- (INCORRECT, COMPILE-TIME ERROR)
+--
+-- sumGenEqual :: (Arbitrary a, Arbitrary b) => Gen (Sum a b)
+-- sumGenEqual = do
+--     a <- arbitrary
+--     b <- arbitrary
+--     oneof [return $ First a, return $ Second b]
+
+-- sumGenFoolFool :: Gen (Sum Fool Fool)
+-- sumGenFoolFool = sumGenEqual
+
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+-- (PERSONAL NOTE: Not quickCheck foolGen, but rather sample foolGen)
+instance Arbitrary Fool where
+    arbitrary = frequency [(1, return Fulse), (1, return Frue)]
+
+foolGen :: Gen Fool
+foolGen = arbitrary
+
+-- (FROM ANSWER KEY)
+fulsishFoolGen :: Gen Fool
+fulsishFoolGen = do
+    a <- arbitrary
+    frequency [(2, return a), (1, return a)]
+
+-- 2) 2/3s chance of Fulse, 1/3 chance of Frue.
