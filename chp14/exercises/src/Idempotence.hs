d3954bd27ae88660caf78cae203b7db8748703aa
Ying Wang <yingw787.personal@gmail.com>
2019-12-19 15:42:12 -0500

    Update Idempotence.hs.

--- chp14/exercises/src/Idempotence.hs
+++ chp14/exercises/src/Idempotence.hs
@@ -1,5 +1,30 @@
 -- Idempotence.hs
 module Idempotence where
 
-sayHello :: IO ()
-sayHello = putStrLn "Hello!"
+import Data.Char (toUpper)
+import Data.List (sort)
+import Test.QuickCheck
+
+twice f = f . f
+fourTimes = twice . twice
+
+-- 1)
+prop_IdemOne :: [Char] -> Bool
+prop_IdemOne x =
+    (capitalizeWord x == twice capitalizeWord x) &&
+    (capitalizeWord x == fourTimes capitalizeWord x) where
+        -- (PERSONAL NOTE: Not sure where method capitalizeWord came from,
+        -- answer key implemented it as below:)
+        capitalizeWord "" = ""
+        capitalizeWord (x : xs) = toUpper x : xs
+
+-- 2)
+prop_IdemTwo :: (Ord a) => [a] -> Bool
+prop_IdemTwo x =
+    (sort x == twice sort x) &&
+    (sort x == fourTimes sort x)
+
+main :: IO ()
+main = do
+    quickCheck (prop_IdemOne :: String -> Bool)
+    quickCheck (prop_IdemTwo :: String -> Bool)
