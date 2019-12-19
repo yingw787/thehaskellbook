8db65ca26519b87463af08cd6450a707c3da2b62
Ying Wang <yingw787.personal@gmail.com>
2019-12-18 20:00:18 -0500

    Update NumbersIntoWords.hs.

--- chp14/exercises/src/NumbersIntoWords.hs
+++ chp14/exercises/src/NumbersIntoWords.hs
@@ -1,6 +1,10 @@
 -- NumbersIntoWords.hs
 module NumbersIntoWords where
 
+import Data.List (intersperse)
+
+import Test.Hspec
+
 -- From WordNumber.hs, pasted here because WordNumber is not part of the same
 -- scope I think
 digitToWord :: Int -> String
@@ -28,22 +32,23 @@ digits n = go n []
 wordNumber :: Int -> String
 wordNumber n = concat (intersperse "-" (map (digitToWord) (digits n)))
 
+-- (CORRECT BY GHCI OUTPUT)
 main :: IO ()
 main = hspec $ do
   describe "digitToWord" $ do
     it "returns zero for 0" $ do
       digitToWord 0 `shouldBe` "zero"
     it "returns one for 1" $ do
-      print "???"
+      digitToWord 1 `shouldBe` "one"
 
   describe "digits" $ do
     it "returns [1] for 1" $ do
       digits 1 `shouldBe` [1]
     it "returns [1, 0, 0] for 100" $ do
-      print "???"
+      digits 100 `shouldBe` [1, 0, 0]
 
   describe "wordNumber" $ do
     it "one-zero-zero given 100" $ do
       wordNumber 100 `shouldBe` "one-zero-zero"
     it "nine-zero-zero-one for 9001" $ do
-      print "???"
+      wordNumber 9001 `shouldBe` "nine-zero-zero-one"
