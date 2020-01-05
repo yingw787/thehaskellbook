43f2741eb2d77e443d611d4c39e0d36a5e30f453
Ying Wang <yingw787.personal@gmail.com>
2020-01-05 17:06:55 -0500

    Finalize OptionalMonoid.hs different from answer key due to upgrade of Haskell Data.Monoid in GHC.Base.

--- chp15/OptionalMonoid.hs
+++ chp15/OptionalMonoid.hs
@@ -8,13 +8,26 @@ data Optional a = Nada | Only a deriving (Eq, Show)
 
 instance Monoid a => Monoid (Optional a) where
     mempty = Nada
-    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
-    mappend Nada Nada = Nada
-    mappend Nada (Only x) = (Only x)
-    mappend (Only x) Nada = (Only x)
-    mappend (Only x) (Only y) = (Only (mappend x y))
 
+-- (FROM ANSWER KEY: https://stackoverflow.com/a/53800966)
+--
+-- As of Haskell base-4.11.0.0, `Semigroup` is a superclass of `Monoid`, and
+-- method `(<>) / mappend` should be defined as part of the `Semigroup`
+-- instance.
+instance Semigroup a => Semigroup (Optional a) where
+    Nada <> Nada = Nada
+    Nada <> (Only a) = Only a
+    (Only a) <> Nada = Only a
+    (Only a) <> (Only a') = Only (a <> a')
+
+-- (CORRECT BY GHCI OUTPUT)
 main :: IO ()
 main = do
-    print "Hello"
-    -- print $ Only (Sum 1) `mappend` Only (Sum 1)
+    -- should be `Only (Sum {getSum = 2})
+    print $ Only (Sum 1) `mappend` Only (Sum 1)
+    -- should be `Only (Product {getProduct = 8})
+    print $ Only (Product 4) `mappend` Only (Product 2)
+    -- should be `Only (Sum {getSum = 1})`
+    print $ Only (Sum 1) `mappend` Nada
+    -- should be `Only [1]`
+    print $ Only [1] `mappend` Nada
