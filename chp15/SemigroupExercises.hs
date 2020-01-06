76c2eb2141dbf9ccc661f32f064c3f51faa96e07
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 11:10:03 -0500

    Update SemigroupExercises.hs.

--- chp15/SemigroupExercises.hs
+++ chp15/SemigroupExercises.hs
@@ -172,6 +172,32 @@ main7 =
     quickCheck (semigroupAssoc :: BoolDisjAssoc)
 
 -- 8)
+--
+-- (PERSONAL NOTE: Going to try to solve it by myself but need to check answer
+-- key afterwards..)
+--
+-- (PERSONAL NOTE: Wow it actually matches, I'm pretty happy)
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+    arbitrary = do
+        a' <- arbitrary
+        b' <- arbitrary
+        oneof [return (Fst a'), return (Snd b')]
+
+instance (Semigroup a, Semigroup b) => Semigroup (Or a b) where
+    (<>) (Snd x) _ = (Snd x)
+    (<>) _ (Snd x) = (Snd x)
+    -- (PERSONAL NOTE: This is correct, answer key has `(<>) _ (Fst x) = (Fst
+    -- x)`)
+    (<>) (Fst x) (Fst x') = (Fst x')
+
+type OrAssoc = (Or String String) -> (Or String String) -> (Or String String) -> Bool
+
+main8 :: IO ()
+main8 =
+    -- (CORRECT BY GHCI OUTPUT)
+    quickCheck (semigroupAssoc :: OrAssoc)
 
 -- 9)
 
