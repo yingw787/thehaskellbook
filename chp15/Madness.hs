09000f19c188cf64ebb6840d84b3714a26704ac3
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 08:26:06 -0500

    Add Madness.hs.

---
+++ chp15/Madness.hs
@@ -0,0 +1,16 @@
+-- Madness.hs
+module Madness where
+
+import Data.Monoid
+
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbin' e adv noun adj =
+    e <> "! he said " <>
+    adv <> " as he jumped into his car " <>
+    noun <> " and drove off with his " <>
+    adj <> " wife."
