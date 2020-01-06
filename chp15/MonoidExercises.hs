5f1b43d376b177ca97eeeefbd8bb69c130b58897
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 11:58:14 -0500

    Update MonoidExercises.hs.

--- chp15/MonoidExercises.hs
+++ chp15/MonoidExercises.hs
@@ -3,8 +3,7 @@ module MonoidExercises where
 
 import Test.QuickCheck
 
-import SemigroupExercises (semigroupAssoc)
-import Check (monoidLeftIdentity, monoidRightIdentity)
+import Check (monoidAssoc, monoidLeftIdentity, monoidRightIdentity)
 
 
 -- 1)
@@ -24,11 +23,34 @@ type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
 
 main1 :: IO ()
 main1 = do
-    quickCheck (semigroupAssoc :: TrivAssoc)
+    quickCheck (monoidAssoc :: TrivAssoc)
     quickCheck (monoidLeftIdentity :: Trivial -> Bool)
     quickCheck (monoidRightIdentity :: Trivial -> Bool)
 
 -- 2)
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = do
+        a' <- arbitrary
+        return (Identity a')
+
+instance Semigroup a => Semigroup (Identity a) where
+    (<>) (Identity a) (Identity b) = Identity (a <> b)
+
+-- (PERSONAL NOTE: Not exactly sure whether I should have divided up this into
+-- Monoid and Semigroup) (Yes I should have because Semigroup must be deduced)
+instance Monoid a => Monoid (Identity a) where
+    mempty = (Identity mempty)
+    mappend = (<>)
+
+type IdentityAssoc = (Identity String) -> (Identity String) -> (Identity String) -> Bool
+
+main2 :: IO ()
+main2 = do
+    quickCheck (monoidAssoc :: IdentityAssoc)
+    quickCheck (monoidLeftIdentity :: Identity String -> Bool)
+    quickCheck (monoidRightIdentity :: Identity String -> Bool)
 
 -- 3)
 
