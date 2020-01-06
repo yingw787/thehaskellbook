babb0bce77adb811a0f896d1b6d60face105ef17
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 10:49:18 -0500

    Update SemigroupExercises.hs.

--- chp15/SemigroupExercises.hs
+++ chp15/SemigroupExercises.hs
@@ -95,7 +95,7 @@ main3 =
 -- 4)
 data Three a b c = Three a b c deriving (Eq, Show)
 
-instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c ) where
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
     arbitrary = do
         a' <- arbitrary
         b' <- arbitrary
@@ -111,3 +111,36 @@ main4 :: IO ()
 main4 =
     -- (CORRECT BY GHCI OUTPUT)
     quickCheck (semigroupAssoc :: ThreeAssoc)
+
+-- 5)
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+    arbitrary = do
+        a' <- arbitrary
+        b' <- arbitrary
+        c' <- arbitrary
+        d' <- arbitrary
+        return (Four a' b' c' d')
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+    (<>) (Four a1 b1 c1 d1) (Four a2 b2 c2 d2) = Four (a1 <> a2) (b1 <> b2) (c1 <> c2) (d1 <> d2)
+
+type FourAssoc = (Four String String String String) -> (Four String String String String) -> (Four String String String String) -> Bool
+
+main5 :: IO ()
+main5 =
+    -- (CORRECT BY GHCI OUTPUT)
+    quickCheck (semigroupAssoc :: FourAssoc)
+
+-- 6)
+
+-- 7)
+
+-- 8)
+
+-- 9)
+
+-- 10)
+
+-- 11)
