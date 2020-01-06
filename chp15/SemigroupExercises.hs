59d0312d9a3110a6a357b255d34140b6c2b97686
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 10:33:15 -0500

    Update SemigroupExercises.hs.

--- chp15/SemigroupExercises.hs
+++ chp15/SemigroupExercises.hs
@@ -29,3 +29,36 @@ type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
 main1 :: IO ()
 main1 =
     quickCheck (semigroupAssoc :: TrivAssoc)
+
+-- 2)
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    -- (INCORRECT, type argument 'a' does not translate to QuickCheck
+    -- generation)
+    --
+    -- arbitrary = return (Identity a)
+    --
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    --
+    arbitrary = do
+        a' <- arbitrary
+        return (Identity a')
+
+instance Semigroup a => Semigroup (Identity a) where
+    -- (PERSONAL NOTE: ...why does an identity newtype have a type argument...?)
+    -- (<>) (Identity a) (Identity b) = (Identity a)
+    --
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    -- (PERSONAL NOTE: How does this method enforce types for inner values? What
+    -- if they're different?) (ANSWER: Through type signature that enforces
+    -- Semigroup typeclass on inner type argument, and identical types for both
+    -- 'a' and 'b'.)
+    --
+    (<>) (Identity a) (Identity b) = Identity (a <> b)
+
+type IdentityAssoc = (Identity String) -> (Identity String) -> (Identity String) -> Bool
+
+main2 :: IO ()
+main2 =
+    quickCheck (semigroupAssoc :: IdentityAssoc)
