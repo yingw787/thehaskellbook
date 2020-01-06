1b6b851fd69f1058a54c077dfa3679cf900b2c94
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 11:48:29 -0500

    Update MonoidExercises.hs.

--- chp15/MonoidExercises.hs
+++ chp15/MonoidExercises.hs
@@ -3,8 +3,30 @@ module MonoidExercises where
 
 import Test.QuickCheck
 
+import SemigroupExercises (semigroupAssoc)
+import Check (monoidLeftIdentity, monoidRightIdentity)
+
 
 -- 1)
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+    (<>) _ _ = Trivial
+
+instance Monoid Trivial where
+    mempty = Trivial
+    mappend = (<>)
+
+instance Arbitrary Trivial where
+    arbitrary = return Trivial
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+main1 :: IO ()
+main1 = do
+    quickCheck (semigroupAssoc :: TrivAssoc)
+    quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+    quickCheck (monoidRightIdentity :: Trivial -> Bool)
 
 -- 2)
 
