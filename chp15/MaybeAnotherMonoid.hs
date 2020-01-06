5d32fd8b627cf5fd07e82716644ae6ce569dc76e
Ying Wang <yingw787.personal@gmail.com>
2020-01-06 09:32:01 -0500

    Update MaybeAnotherMonoid.hs.

--- chp15/MaybeAnotherMonoid.hs
+++ chp15/MaybeAnotherMonoid.hs
@@ -1,13 +1,24 @@
 -- MaybeAnotherMonoid.hs
 module MaybeAnotherMonoid where
 
+import Control.Monad
+import Data.Monoid
+import Test.QuickCheck
+
+import OptionalMonoid
+import Check (monoidAssoc, monoidLeftIdentity, monoidRightIdentity)
+
 newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
 
 instance Semigroup (First' a) where
-    (<>) = undefined
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    (<>) (First' Nada) (First' Nada) = (First' Nada)
+    (<>) (First' Nada) (First' (Only x)) = (First' (Only x))
+    (<>) (First' (Only x)) _ = (First' (Only x))
 
 instance Monoid (First' a) where
-    mempty = undefined
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    mempty = First' Nada
 
 firstMappend :: First' a -> First' a -> First' a
 firstMappend = mappend
