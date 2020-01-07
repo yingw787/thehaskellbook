57e59b03e5306022741108906f2a56ba33440379
Ying Wang <yingw787.personal@gmail.com>
2020-01-07 11:34:51 -0500

    Update ReplaceExperiement.hs.

--- chp16/ReplaceExperiment.hs
+++ chp16/ReplaceExperiment.hs
@@ -18,3 +18,39 @@ liftedReplace = fmap replaceWithP
 -- More specific functor '[]', as in '[Char]'.
 liftedReplace' :: [Maybe [Char]] -> [Char]
 liftedReplace' = liftedReplace
+
+twiceLifted :: (Functor f1, Functor f) => f (f1 a) -> f (f1 Char)
+twiceLifted = (fmap . fmap) replaceWithP
+
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' = twiceLifted
+
+thriceLifted :: (Functor f2, Functor f1, Functor f) => f (f1 (f2 a)) -> f (f1 (f2 Char))
+thriceLifted = (fmap . fmap . fmap) replaceWithP
+
+thriceLifted' :: [Maybe [Char]] -> [Maybe [Char]]
+thriceLifted' = thriceLifted
+
+
+main :: IO ()
+main = do
+    putStr "replaceWithP' lms: "
+    print $ replaceWithP' lms
+
+    putStr "liftedReplace lms: "
+    print $ liftedReplace lms
+
+    putStr "liftedReplace' lms: "
+    print $ liftedReplace' lms
+
+    putStr "twiceLifted lms: "
+    print $ twiceLifted lms
+
+    putStr "twiceLifted' lms: "
+    print $ twiceLifted' lms
+
+    putStr "thriceLifted lms: "
+    print $ thriceLifted lms
+
+    putStr "thriceLiftd' lms: "
+    print $ thriceLifted' lms
