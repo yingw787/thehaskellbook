f262819f9e6d6f376cd77979605fb7939c6b8da5
Ying Wang <yingw787.personal@gmail.com>
2020-01-08 17:21:26 -0500

    Update ConstantInstance.hs.

--- chp17/ConstantInstance.hs
+++ chp17/ConstantInstance.hs
@@ -1,4 +1,7 @@
 -- ConstantInstance.hs
+--
+-- (PERSONAL NOTE: Honestly, first pass around, I'm not quite sure how to work
+-- through this. All diffs are from answer key.)
 module ConstantInstance where
 
 
@@ -7,8 +10,23 @@ newtype Constant a b =
     deriving (Eq, Ord, Show)
 
 instance Functor (Constant a) where
-    fmap = undefined
+    -- (FROM ANSWER KEY)
+    --
+    -- (PERSONAL NOTE: 'f' is not applied, this is why 'Constant' is used when
+    -- you need to elide a function application)
+    fmap f (Constant a) = (Constant a)
 
 instance Monoid a => Applicative (Constant a) where
-    pure = undefined
-    (<*>) = undefined
+    -- (FROM ANSWER KEY)
+    --
+    -- (PERSONAL NOTE: 'mempty' appears to be a sane default for an arbitrary
+    -- type argument, to be defined in a concrete type somewhere else. I thought
+    -- it was tightly coupled to Monoid for some reason...) (I still might be
+    -- wrong...)
+    pure f = (Constant mempty)
+    --
+    -- (I tried to implement (<*>), this is incorrect)
+    --
+    -- (<*>) (Constant f) (Constant a) = (Constant a)
+    --
+    (<*>) (Constant a) (Constant b) = Constant (mappend a b)
