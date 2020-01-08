3aa7a7ff612732470ea2259639b4515f1631073b
Ying Wang <yingw787.personal@gmail.com>
2020-01-08 10:30:23 -0500

    Add partial Written.hs.

--- chp16/Written.hs
+++ chp16/Written.hs
@@ -1,10 +1,45 @@
 {-# LANGUAGE FlexibleInstances #-}
 -- Written.hs
+--
+-- No answer key for this section, rely on Test.QuickCheck for correctness.
 module Written where
+
+import Test.QuickCheck
+-- For 'Data.Fun'.
+import Test.QuickCheck.Function
+
+
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f = fmap id f == f
+
+functorCompose :: (Eq (f c), Functor f) => (a -> b)-> (b -> c) -> f a -> Bool
+functorCompose f g x = (fmap g (fmap f x)) == (fmap (g . f) x)
+
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+type IntToInt = Fun Int Int
+
+-- Usage for quickCheck functions.
+functorCompose' :: (Eq (f c), Functor f) => Fun a b -> Fun b c -> f a -> Bool
+functorCompose' (Fun _ f) (Fun _ g) x = (fmap g (fmap f x)) == (fmap (g . f) x)
 
 
 -- 1)
-data Quant a b = Finance | Desk a | Floor b
+--
+-- (CORRECT BY GHCI OUTPUT)
+data Quant a b = Finance | Desk a | Floor b deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Quant a b) where
+    arbitrary = do
+        a' <- arbitrary
+        b' <- arbitrary
+        oneof [return $ Finance, return $ Desk a', return $ Floor b']
+
+-- Parentheses are important in order to avoid 'fmap has different numbers of
+-- arguments' error.
+instance Functor (Quant e) where
+    fmap _ Finance = Finance
+    fmap _ (Desk a) = Desk a
+    fmap f (Floor b) = Floor (f b)
 
 -- 2)
 data K a b = K a
@@ -40,3 +75,9 @@ data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (Goat
 
 -- 11)
 data TalkToMe a = Halt | Print String a | Read (String -> a)
+
+
+main :: IO ()
+main = do
+    quickCheck (functorIdentity :: (Quant Int Int) -> Bool)
+    quickCheck (functorCompose' :: IntToInt -> IntToInt -> (Quant Int Int) -> Bool)
