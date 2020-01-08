6ae7e2378a5c5ba1847b8009e3a4b1ed385ae62b
Ying Wang <yingw787.personal@gmail.com>
2020-01-08 09:28:56 -0500

    Update IsValid.hs.

--- chp16/IsValid.hs
+++ chp16/IsValid.hs
@@ -5,16 +5,36 @@ import GHC.Arr
 
 
 -- 1)
+--
+-- No, a valid Functor instance cannot be written, because Bool has kind '*'.
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
 data Bool = False | True
 
 -- 2)
+--
+-- Yes, a valid Functor instance can be written, kind is '* -> *'.
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
 data BoolAndSomethingElse a = False' a | True' a
 
 -- 3)
+--
+-- Yes, kind is '* -> *'.
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
 data BoolAndMaybeSomethingElse a = Falsish | Truish a
 
 -- 4)
+--
+-- Yes, kind is '(* -> *) -> *'.
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
 newtype Mu f = InF { outF :: f (Mu f) }
 
 -- 5)
+--
+-- No, kind is '*'.
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
 data D = D (Array Word Word) Int Int
