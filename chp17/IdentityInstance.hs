6c5c411cdd8b725e332d0da1123340cfd14a4866
Ying Wang <yingw787.personal@gmail.com>
2020-01-08 16:58:58 -0500

    Update IdentityInstance.hs.

--- chp17/IdentityInstance.hs
+++ chp17/IdentityInstance.hs
@@ -4,8 +4,20 @@ module IdentityInstance where
 newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
 instance Functor Identity where
-    fmap = undefined
+    -- fmap = id
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    fmap f (Identity a) = Identity (f a)
 
 instance Applicative Identity where
-    pure = undefined
-    (<*>) = undefined
+    -- pure = id
+    -- x <*> Identity = x
+    -- Identity <*> x = x
+    --
+    -- (INCORRECT, I'm not exactly sure why)
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    --
+    -- (PERSONAL NOTE: I think that 'Identity', because there is only one data
+    -- constructor, provides the consistent functorial structure to pass through
+    -- any methods to be applied to the internal value)
+    pure f = (Identity f)
+    (<*>) (Identity f) (Identity a) = Identity (f a)
