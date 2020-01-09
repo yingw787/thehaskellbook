01cab7c8e9c319542f312d88f6d591a0489f1a30
Ying Wang <yingw787.personal@gmail.com>
2020-01-09 17:20:40 -0500

    Update Instances.hs.

--- chp17/Instances.hs
+++ chp17/Instances.hs
@@ -39,6 +39,40 @@ main1 = do
     quickBatch $ applicative (Pair test test)
 
 -- 2)
+data Two a b = Two a b deriving (Eq, Show)
+
+-- (CORRECT BY CHECKING ANSWER KEY)
+instance Functor (Two a) where
+    fmap f (Two a b) = Two a (f b)
+
+-- (INCORRECT BY QUICKBATCH CHECK)
+--
+-- instance (Monoid a) => Applicative (Two a) where
+--     pure a = Two mempty a
+--     (<*>) (Two f g) (Two a b) = Two a (g b)
+--
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+--
+instance Monoid a => Applicative (Two a) where
+    pure a = Two mempty a
+    (<*>) (Two a b) (Two c d) = Two (mappend a c) (b d)
+
+-- (CORRECT BY CHECKING ANSWER KEY)
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+    arbitrary = do
+        a' <- arbitrary
+        b' <- arbitrary
+        return (Two a' b')
+
+-- (CORRECT BY CHECKING ANSWER KEY)
+instance (Eq a, Eq b) => EqProp (Two a b) where
+    (=-=) = eq
+
+
+main2 :: IO ()
+main2 = do
+    let test = ("a", "b", "c")
+    quickBatch $ applicative (Two test test)
 
 -- 3)
 
