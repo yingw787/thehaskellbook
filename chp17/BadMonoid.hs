1a816c6ff27f22a2762e4337d8eaf7c10b17cb93
Ying Wang <yingw787.personal@gmail.com>
2020-01-09 11:47:29 -0500

    Update BadMonoid.hs.

--- chp17/BadMonoid.hs
+++ chp17/BadMonoid.hs
@@ -12,3 +12,26 @@ import Data.Monoid
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
+
+
+data Bull = Fools | Twoo deriving (Eq, Show)
+
+instance Arbitrary Bull where
+    arbitrary =
+        frequency [ (1, return Fools), (1, return Twoo) ]
+
+instance Semigroup Bull where
+    (<>) _ _ = Fools
+
+instance Monoid Bull where
+    mempty = Fools
+    mappend = (<>)
+
+-- `EqProp` is from library `checkers`
+instance EqProp Bull where
+    (=-=) = eq
+
+
+main :: IO ()
+main = do
+    quickBatch (monoid Twoo)
