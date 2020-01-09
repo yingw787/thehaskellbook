fa45af30b3e885b1301558dff6b42637bd0fbb60
Ying Wang <yingw787.personal@gmail.com>
2020-01-09 12:21:36 -0500

    Update Apl1.hs got it to compile after checking answer key.

--- chp17/Apl1.hs
+++ chp17/Apl1.hs
@@ -9,10 +9,9 @@ import Test.QuickCheck.Classes
 
 
 -- this isn't going to work properly
---
--- (PERSONAL NOTE: Not sure how the author got this to compile, but I get a
--- namespacing error when importing module 'Test.QuickCheck' because it collides
--- with arbitrary instance declaration for 'ZipList'...)
+instance Monoid a => Semigroup (ZipList a) where
+    (<>) = liftA2 mappend
+
 instance Monoid a => Monoid (ZipList a) where
     -- This won't work because the empty `ZipList` is the zero and not the
     -- identity!
@@ -22,13 +21,21 @@ instance Monoid a => Monoid (ZipList a) where
     -- Change to:
     --
     mempty = pure mempty
-    mappend = liftA2 mappend
+    mappend = (<>)
 
-instance Arbitrary a => Arbitrary (ZipList a) where
-    arbitrary = ZipList <$> arbitrary
+-- (PERSONAL NOTE: Not sure how the author got this to compile, but I get a
+-- namespacing error when importing module 'Test.QuickCheck' because it collides
+-- with arbitrary instance declaration for 'ZipList'...)
+--
+-- (PERSONAL NOTE: I checked the answer key from
+-- https://github.com/johnchandlerburnham/hpfp/ and these portions of the module
+-- are commented out, so I will comment them out)
+--
+-- instance Arbitrary a => Arbitrary (ZipList a) where
+--     arbitrary = ZipList <$> arbitrary
 
-instance Arbitrary a => Arbitrary (Sum a) where
-    arbitrary = Sum <$> arbitrary
+-- instance Arbitrary a => Arbitrary (Sum a) where
+--     arbitrary = Sum <$> arbitrary
 
 instance Eq a => EqProp (ZipList a) where
     (=-=) = eq
