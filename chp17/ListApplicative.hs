6671233578b44fceda203f1935d49651a7f3b2a1
Ying Wang <yingw787.personal@gmail.com>
2020-01-09 13:52:42 -0500

    Update ListApplicative.hs.

--- chp17/ListApplicative.hs
+++ chp17/ListApplicative.hs
@@ -13,3 +13,26 @@ import Test.QuickCheck.Classes
 
 
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where
+    fmap = undefined
+
+instance Applicative List where
+    pure = undefined
+    (<*>) = undefined
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys =
+    Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+-- write this one in terms of concat' and fmap
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = undefined
