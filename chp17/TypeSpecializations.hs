a405896b1001cfdd4dc0a1cc0673070b10ad1148
Ying Wang <yingw787.personal@gmail.com>
2020-01-09 16:51:35 -0500

    Update TypeSpecializations.hs.

--- chp17/TypeSpecializations.hs
+++ chp17/TypeSpecializations.hs
@@ -19,3 +19,40 @@ listApply :: [(a -> b)] -> [a] -> [b]
 listApply = (<*>)
 
 -- 2)
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
+ioPure :: a -> IO a
+ioPure = pure
+
+ioApply :: IO (a -> b) -> IO a -> IO b
+ioApply = (<*>)
+
+-- 3)
+--
+-- (INCORRECT, COMPLIATION ERROR)
+--
+-- tuplePure :: a -> (,) a
+-- tuplePure = pure
+--
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+-- (PERSONAL NOTE: I don't understand this solution. I thought the whole point
+-- of 'Control.Applicative' was that the function would be applied to the
+-- arguments (monoidal)? Why is it present in the final result?)
+--
+tuplePure :: (Monoid a, Monoid c) => c -> (c, a)
+tuplePure = pure
+
+-- (PERSONAL NOTE: Not sure how to reduce the kind-ness of (,).)
+--
+-- (INCORRECT, GHC COMPILATION ERROR)
+--
+-- tupleApply :: (,) (a -> b) -> (,) a -> (,) b
+-- tupleApply = (<*>)
+--
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+-- (PERSONAL NOTE: I think 'b' doesn't need the Monoid constraint because only a
+-- needs to be combinable.)
+--
+tupleApply :: (Monoid a, Monoid c) => (c, (a -> b)) -> (c, a) -> (c, b)
+
+-- 4)
