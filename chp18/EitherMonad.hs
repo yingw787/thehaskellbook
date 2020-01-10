5c9e3223d8f8671c4bb54e8e568427562d7d594f
Ying Wang <yingw787.personal@gmail.com>
2020-01-10 18:36:47 -0500

    Update EitherMonad.hs.

--- chp18/EitherMonad.hs
+++ chp18/EitherMonad.hs
@@ -29,3 +29,25 @@ data FoundedError =
     | TooManyCoders Coders
     | TooManyCodersForYears Founded Coders
     deriving (Eq, Show)
+
+validateFounded :: Int -> Either FoundedError Founded
+validateFounded n
+    | n < 0 = Left $ NegativeYears n
+    | n > 500 = Left $ TooManyYears n
+    | otherwise = Right n
+
+-- Tho, many programmers *are* negative.
+validateCoders :: Int -> Either FoundedError Coders
+validateCoders n
+    | n < 0 = Left $ NegativeCoders n
+    | n > 5000 = Left $ TooManyCoders n
+    | otherwise = Right n
+
+mkSoftware :: Int -> Int -> Either FoundedError SoftwareShop
+mkSoftware years coders = do
+    founded <- validateFounded years
+    programmers <- validateCoders coders
+
+    if programmers > div founded 10
+        then Left $ TooManyCodersForYears founded programmers
+        else Right $ Shop founded programmers
