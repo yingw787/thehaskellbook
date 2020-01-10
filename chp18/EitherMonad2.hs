40528fc10899438e7d9729caa9daa9f6c04f99b5
Ying Wang <yingw787.personal@gmail.com>
2020-01-10 18:59:02 -0500

    Add EitherMonad2.hs.

--- chp18/EitherMonad2.hs
+++ chp18/EitherMonad2.hs
@@ -4,13 +4,32 @@ module EitherMonad2 where
 
 data Sum a b = First a | Second b deriving (Eq, Show)
 
+-- (CORRECT BY CHECKING ANSWER KEY)
 instance Functor (Sum a) where
-    fmap = undefined
+    fmap _ (First a) = First a
+    fmap f (Second b) = Second (f b)
 
 instance Applicative (Sum a) where
-    pure = undefined
-    (<*>) = undefined
+    -- (MAY NOT BE CORRECT)
+    --
+    -- pure = Second
+    --
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    --
+    pure b = Second b
+    --
+    -- (CORRECT BY CHECKING ANSWER KEY)
+    --
+    (First a) <*> _ = First a
+    _ <*> (First a) = First a
+    (Second a) <*> (Second b) = Second (a b)
 
 instance Monad (Sum a) where
     return = pure
-    (>>=) = undefined
+    --
+    -- (PERSONAL NOTE: Not sure how to start with this...)
+    --
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    -- (PERSONAL NOTE: Wait, 'bind' removes monadic structure?)
+    --
+    (>>=) (Second b) f = (f b)
