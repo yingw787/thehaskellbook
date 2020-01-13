d9078a2ad3eeef7216471facc5bb9e26d8a4e3e7
Ying Wang <yingw787.personal@gmail.com>
2020-01-13 18:02:59 -0500

    Update MonadFunctions.hs not sure how to proceed on these methods, will come back to them later.

--- chp18/MonadFunctions.hs
+++ chp18/MonadFunctions.hs
@@ -1,4 +1,7 @@
 -- MonadFunctions.hs
+--
+-- (PERSONAL NOTE: Just going to back away slowly here. Not ready to work on
+-- these functions yet.)
 module MonadFunctions where
 
 import Control.Monad
@@ -8,3 +11,22 @@ import Test.QuickCheck.Classes
 
 
 -- 1)
+j :: Monad m => m (m a) -> m a
+j = undefined
+
+-- main1 :: IO ()
+-- main1 = do
+--     print $ j [[1, 2], [], [3]]
+--     print $ j (Just (Just 1))
+--     print $ j (Just Nothing)
+--     print $ j Nothing
+
+-- 2)
+
+-- 3)
+
+-- 4)
+
+-- 5)
+
+-- 6)
