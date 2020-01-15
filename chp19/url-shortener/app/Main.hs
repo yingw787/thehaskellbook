b44ff33297e3a3b847fdcb8291e29fbc35f8d763
Ying Wang <yingw787.personal@gmail.com>
2020-01-15 18:34:31 -0500

    Update Main.hs.

--- chp19/url-shortener/app/Main.hs
+++ chp19/url-shortener/app/Main.hs
@@ -25,6 +25,20 @@
 -- executable.
 module Main where
 
+import Control.Monad (replicateM)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Char8 as BC
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.Text.Lazy as TL
+import qualified Database.Redis as R
+import Network.URI (URI, parseURI)
+import qualified System.Random as SR
+import Web.Scotty
+
+
+alphaNum :: String
+alphaNum = ['A'.'Z'] ++ ['0'..'9']
+
 main :: IO ()
 main = do
   putStrLn "hello world"
