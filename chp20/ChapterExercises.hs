a15e6e7d0423cb41fddd8186c5433c003030674d
Ying Wang <yingw787.personal@gmail.com>
2020-01-16 18:07:24 -0500

    Update ChapterExercises.md.

--- chp20/ChapterExercises.hs
+++ chp20/ChapterExercises.hs
@@ -30,10 +30,30 @@ instance Foldable (Two a) where
     foldMap f (Two a b) = f b
 
 -- 3)
+--
+-- (CORRECT BY CHECKING ANSWER KEY)
+-- (PERSONAL NOTE: I think what I'm getting is higher-kinded types)
+data Three a b c = Three a b c deriving Show
+
+instance Foldable (Three a b) where
+    foldMap f (Three a b c) = f c
 
 -- 4)
+data Three' a b = Three' a b b deriving Show
+
+-- (CORRECT BY CHECKING ANSWER KEY, GHCI OUTPUT AND COMPILE SUCCESS)
+-- (PERSONAL NOTE: I was extremely surprised that this worked. I'm not sure why
+-- but it just felt like the right solution. I don't think I understand this
+-- intuitvely.)
+instance Foldable (Three' a) where
+    foldMap f (Three' a b c) = (<>) (f b) (f c)
 
 -- 5)
+data Four' a b = Four' a b b b deriving Show
+
+-- (CORRECT BY CHECKING ANSWER KEY, GHCI OUTPUT, AND COMPILE SUCCESS)
+instance Foldable (Four' a) where
+    foldMap f (Four' a b c d) = (f b) <> (f c) <> (f d)
 
 
 main :: IO ()
@@ -41,3 +61,9 @@ main = do
     print $ foldr (*) 5 (Constant 5)
 
     print $ foldMap Sum (Two 5 5)
+
+    print $ foldMap Sum (Three 1 2 3)
+
+    print $ foldMap Sum (Three' 1 2 3)
+
+    print $ foldMap Sum (Four' 1 2 3 4)
