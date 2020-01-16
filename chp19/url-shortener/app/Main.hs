6adade741ceadaf030c4ee54059da6ca7633f22f
Ying Wang <yingw787.personal@gmail.com>
2020-01-15 19:09:17 -0500

    Update Main.hs.

--- chp19/url-shortener/app/Main.hs
+++ chp19/url-shortener/app/Main.hs
@@ -37,7 +37,7 @@ import Web.Scotty
 
 
 alphaNum :: String
-alphaNum = ['A'.'Z'] ++ ['0'..'9']
+alphaNum = ['A'..'Z'] ++ ['0'..'9']
 
 randomElement :: String -> IO Char
 randomElement xs = do
@@ -81,7 +81,7 @@ shortyCreated resp shawty =
             , TL.pack (linkShorty shawty)
             ]
 
-shortyAintUri :: TL.Text > TL.Text
+shortyAintUri :: TL.Text -> TL.Text
 shortyAintUri uri =
   TL.concat
     [ uri
@@ -100,8 +100,36 @@ app :: R.Connection -> ScottyM ()
 app rConn = do
   get "/" $ do
     uri <- param "uri"
+    let parsedUri :: Maybe URI
+        parsedUri = parseURI (TL.unpack uri)
+
+    case parsedUri of
+      Just _ -> do
+        shawty <- liftIO shortyGen
+        let shorty = BC.pack shawty
+            uri' =
+              encodeUtf8 (TL.toStrict uri)
+        resp <-
+          liftIO (saveURI rConn shorty uri')
+        html (shortyCreated resp shawty)
+      Nothing -> text (shortyAintUri uri)
+
+  get "/:short" $ do
+    short <- param "short"
+    uri <- liftIO (getURI rConn short)
+
+    case uri of
+      Left reply ->
+        text (TL.pack (show reply))
+      Right mbBS -> case mbBS of
+        Nothing -> text "uri not found"
+        Just bs -> html (shortyFound tbs) where
+          tbs :: TL.Text
+          tbs = TL.fromStrict (decodeUtf8 bs)
+
 
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  rConn <- R.connect R.defaultConnectInfo
+  scotty 3000 (app rConn)
