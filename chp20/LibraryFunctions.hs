e972897b5c4b578013946f9be245a205ba5d2efd
Ying Wang <yingw787.personal@gmail.com>
2020-01-16 16:14:20 -0500

    Add partial LibraryFunctions.hs.

---
+++ chp20/LibraryFunctions.hs
@@ -0,0 +1,26 @@
+-- LibraryFunctions.hs
+module LibraryFunctions where
+
+import Data.Foldable
+import Data.Monoid
+
+
+-- 1)
+
+-- 2)
+
+-- 3)
+
+-- 4)
+
+-- 5)
+
+-- 6)
+
+-- 7)
+
+-- 8)
+
+-- 9)
+
+-- 10)
