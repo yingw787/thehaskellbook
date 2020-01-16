0b34caed99f3f6f880073ffca8db861e8995a59d
Ying Wang <yingw787.personal@gmail.com>
2020-01-16 17:40:00 -0500

    Update ChapterExercises.md.

--- chp20/ChapterExercises.hs
+++ chp20/ChapterExercises.hs
@@ -6,6 +6,21 @@ import Data.Monoid
 
 
 -- 1)
+data Constant a b = Constant b
+-- (PERSONAL NOTE: I know I need to implement `foldr` or `foldMap` but I don't
+-- know where to start...) (Hmm looking at my notes from this chapter
+-- helps...lemme give it a shot...)
+
+-- instance Foldable (Constant a) where
+--     foldr f z (Constant b) = f b z
+--     foldl f z (Constant b) = f z b
+--     foldMap f (Constant b) = f b
+--
+-- (INCORRECT BY GHCI OUTPUT; VALUES SHOULD BE HELD CONSTANT)
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+--
+instance Foldable (Constant a) where
+    foldMap _ _ = mempty
 
 -- 2)
 
@@ -14,3 +29,8 @@ import Data.Monoid
 -- 4)
 
 -- 5)
+
+
+main :: IO ()
+main = do
+    print $ foldr (*) 5 (Constant 5)
