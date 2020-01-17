85a004f00951404699fdfd0975c7422c797ddcdf
Ying Wang <yingw787.personal@gmail.com>
2020-01-17 15:54:21 -0500

    Add partial ChapterExercises.hs.

---
+++ chp21/ChapterExercises.hs
@@ -0,0 +1,22 @@
+-- ChapterExercises.hs
+module ChapterExercises where
+
+-- 1) Identity
+
+-- 2) Constant
+
+-- 3) Maybe
+
+-- 4) List
+
+-- 5) Three
+
+-- 6) Pair
+
+-- 7) Big
+
+-- 8) Bigger
+
+-- 9) S
+
+-- 10) Tree
