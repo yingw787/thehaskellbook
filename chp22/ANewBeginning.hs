d607aa858756353e421e7604ecee7e410bf44b1a
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 16:04:02 -0500

    Update ANewBeginning.hs.

--- chp22/ANewBeginning.hs
+++ chp22/ANewBeginning.hs
@@ -7,5 +7,20 @@ import Control.Applicative
 boop = (*2)
 doop = (+10)
 
+-- Will be monomorphic if loaded from a file, polymorphic would involve changing
+-- the type signature.
 bip :: Integer -> Integer
 bip = boop . doop
+
+-- You can `fmap` a function over another function. Functorial context is a
+-- partially applied function.
+--
+-- Example: `fmap boop doop x == (*2) ((+10) x)`
+bloop :: Integer -> Integer
+bloop = fmap boop doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
