2165a4cc2abfb7125418b1a9a9135a11d8cea651
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 16:31:39 -0500

    Add partial WarmingUp.hs.

---
+++ chp22/WarmingUp.hs
@@ -0,0 +1,18 @@
+-- WarmingUp.hs
+module WarmingUp where
+
+import Data.Char
+
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+
+composed :: [Char] -> [Char]
+composed = undefined
+
+fmapped :: [Char] -> [Char]
+fmapped = undefined
