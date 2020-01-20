d98f8befb6a5438faa895695804a44bdd408af27
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 18:59:44 -0500

    Update ReaderMonad.hs.

--- chp22/ReaderMonad.hs
+++ chp22/ReaderMonad.hs
@@ -5,8 +5,21 @@ module ReaderMonad where
 
 newtype Reader r a = Reader { getReader :: r -> a }
 
+-- From chp22/ReadingComprehension.hs
+instance Functor (Reader r) where
+    fmap f (Reader g)= Reader $ f . g
+
+instance Applicative (Reader r) where
+    pure :: a -> Reader r a
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    pure a = Reader $ const a
+
+    (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    (<*>) (Reader rab) (Reader ra) = Reader $ \r -> rab r (ra r)
+
 instance Monad (Reader r) where
     return = pure
-
     (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
-    (>>=) (Reader ra) (aRb) = undefined
+    -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
+    (>>=) (Reader ra) (aRb) = Reader $ \r -> getReader (aRb (ra r)) r
