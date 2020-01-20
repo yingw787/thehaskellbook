b515341c86cb1adbb10d9e068ad6e6631d165ef3
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 13:38:41 -0500

    Update ChapterExercises.hs.

--- chp21/ChapterExercises.hs
+++ chp21/ChapterExercises.hs
@@ -6,10 +6,34 @@ import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
 
+-- Concrete type for testing.
+type IIL = (Int, Int, [Int])
+
 -- 1) Identity
+--
+-- (PERSONAL NOTE: Looking at answer key to figure out how to get started)
+-- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
 newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
 
+instance Foldable Identity where
+    foldMap f (Identity a) = (f a)
+
+instance Traversable Identity where
+    traverse f (Identity a) = fmap Identity (f a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = Identity <$> arbitrary
+
+instance Eq a => EqProp (Identity a) where
+    (=-=) = eq
+
+
+main1 :: IO ()
+main1 = do
+    quickBatch (traversable (undefined :: Identity IIL))
 
 -- 2) Constant
 
