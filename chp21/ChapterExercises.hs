01cf16174902ffa163c4e536dade6fa64d3c26a7
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 14:45:26 -0500

    Update ChapterExercises.hs.

--- chp21/ChapterExercises.hs
+++ chp21/ChapterExercises.hs
@@ -204,6 +204,28 @@ main7 = do
     quickBatch (traversable (undefined :: Big IIL IIL))
 
 -- 8) Bigger
+--
+-- (CORRECT BY GHCI OUTPUT)
+data Bigger a b = Bigger a b b b deriving (Eq, Ord, Show)
+
+instance Functor (Bigger a) where
+    fmap f (Bigger a b c d) = Bigger a (f b) (f c) (f d)
+
+instance Foldable (Bigger a) where
+    foldMap f (Bigger a b c d) = (f b) <> (f c) <> (f d)
+
+instance Traversable (Bigger a) where
+    traverse f (Bigger a b c d) = (Bigger a) <$> (f b) <*> (f c) <*> (f d)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Bigger a b) where
+    arbitrary = Bigger <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Bigger a b) where
+    (=-=) = eq
+
+main8 :: IO ()
+main8 = do
+    quickBatch (traversable (undefined :: Bigger IIL IIL))
 
 -- 9) S
 
