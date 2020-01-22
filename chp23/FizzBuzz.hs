7733590a4c1bc76d8092473140cec800d238f027
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 18:42:05 -0500

    Update FizzBuzz.hs.

--- chp23/FizzBuzz.hs
+++ chp23/FizzBuzz.hs
@@ -1,6 +1,9 @@
 -- FizzBuzz.hs
 module FizzBuzz where
 
+import Control.Monad
+import Control.Monad.Trans.State
+
 
 fizzBuzz :: Integer -> String
 fizzBuzz n
@@ -9,7 +12,20 @@ fizzBuzz n
     | mod n 3 == 0 = "Fizz"
     | otherwise = show n
 
+fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList list =
+    execState (mapM_ addResult list) []
+
+addResult :: Integer -> State [String] ()
+addResult n = do
+    xs <- get
+    let result = fizzBuzz n
+    put (result : xs)
+
 
 main :: IO ()
-main =
+main = do
+    -- Identical output; latter collects data before dumping results to stdout,
+    -- but it also reverses a list
     mapM_ (putStrLn . fizzBuzz) [1..100]
+    mapM_ putStrLn $ reverse $ fizzbuzzList [1..100]
