a69e07e2e95468d8f1e6a675e38c5026a95fe043
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 16:38:40 -0500

    Add partial RandomExample2.hs.

---
+++ chp23/RandomExample2.hs
@@ -0,0 +1,8 @@
+-- RandomExample2.hs
+module RandomExample2 where
+
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import System.Random
+import RandomExample
