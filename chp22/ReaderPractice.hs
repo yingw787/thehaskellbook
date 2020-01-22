90b47bc9f8c9f74facfe889356cabf60212b18eb
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 14:45:15 -0500

    Update ReaderPractice.hs.

--- chp22/ReaderPractice.hs
+++ chp22/ReaderPractice.hs
@@ -8,3 +8,21 @@ import Data.Maybe
 x = [1, 2, 3]
 y = [4, 5, 6]
 z = [7, 8, 9]
+
+lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+-- zip x and y using 3 as the lookup key
+xs :: Maybe Integer
+xs = undefined
+
+-- zip y and z using 6 as the lookup key
+ys :: Maybe Integer
+ys = undefined
+
+-- zip x and y using 4 as the lookup key
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+-- now zip x and z using a variable lookup key
+z' :: Integer -> Maybe Integer
+z' n = undefined
