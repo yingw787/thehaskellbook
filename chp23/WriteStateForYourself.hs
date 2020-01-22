b463568e8b2350375738c2318594ea19cb630f70
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 18:14:53 -0500

    Update WriteStateForYourself.hs.

--- chp23/WriteStateForYourself.hs
+++ chp23/WriteStateForYourself.hs
@@ -13,3 +13,18 @@ instance Functor (Moi s) where
     -- (FROM ANSWER KEY: https://github.com/johnchandlerburnham/hpfp)
     --
     fmap f (Moi g) = Moi $ \s0 -> let (a, s1) = (g s0) in (f a, s1)
+
+-- Implement the 'Applicative' instance for 'State'.
+instance Applicative (Moi s) where
+    pure :: a -> Moi s a
+    pure a = undefined
+
+    (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+    (<*>) (Moi f) (Moi g) = undefined
+
+-- Implement the 'Monad' instance for 'State'.
+instance Monad (Moi s) where
+    return = pure
+
+    (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+    (>>=) (Moi f) g = undefined
