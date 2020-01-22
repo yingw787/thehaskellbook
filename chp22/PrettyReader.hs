adf8c1d6e6a0f92e759a67ec3ad5d4eab120e4ae
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 14:31:39 -0500

    Update PrettyReader.hs.

--- chp22/PrettyReader.hs
+++ chp22/PrettyReader.hs
@@ -5,3 +5,33 @@ module PrettyReader where
 
 flip :: (a -> b -> c) -> (b -> a -> c)
 flip f a b = f b a
+
+const :: a -> b -> a
+const a b = a
+
+(.) :: (b -> c) -> (a -> b) -> (a -> c)
+f . g = \a -> f (g a)
+
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
+
+class Functor f => Applicative f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+class Applicative f => Monad f where
+    return :: a -> f a
+
+    (>>=) :: f a -> (a -> f b) -> f b
+
+instance Functor ((->) r) where
+    fmap = (.)
+
+instance Applicative ((->) r) where
+    pure = const
+
+    f <*> a = \r -> f r (a r)
+
+instance Monad ((->) r) where
+    return = pure
+    m >>= k = flip k <*> m
