9afef30503f850056942f371b1cccccc4506dcb5
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 16:36:31 -0500

    Update RandomExample.hs.

--- chp23/RandomExample.hs
+++ chp23/RandomExample.hs
@@ -5,3 +5,33 @@ import System.Random
 
 
 -- Six-sided die
+data Die =
+    DieOne
+    | DieTwo
+    | DieThree
+    | DieFour
+    | DieFive
+    | DieSix
+    deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+    case n of
+        1 -> DieOne
+        2 -> DieTwo
+        3 -> DieThree
+        4 -> DieFour
+        5 -> DieFive
+        6 -> DieSix
+        x ->
+            -- Don't use 'error' outside of PoCs like this
+            error $
+                "intToDie got non 1-6 integer: " ++ show x
+
+rollDieThreeTimes :: (Die, Die, Die)
+rollDieThreeTimes = do
+    let s = mkStdGen 0
+        (d1, s1) = randomR (1, 6) s
+        (d2, s2) = randomR (1, 6) s1
+        (d3, _) = randomR (1, 6) s2
+    (intToDie d1, intToDie d2, intToDie d3)
