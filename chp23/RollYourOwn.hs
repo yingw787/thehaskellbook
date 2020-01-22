08e53fef9592b7d826622c082b2c9255480fdae5
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 17:18:40 -0500

    Add partial RollYourOwn.hs.

---
+++ chp23/RollYourOwn.hs
@@ -0,0 +1,11 @@
+-- RollYourOwn.hs
+module RollYourOwn where
+
+
+-- 1)
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN = undefined
+
+-- 2)
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged = undefined
