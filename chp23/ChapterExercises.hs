36adac82d618d02cfaf27ddfa6e34bb761510fb7
Ying Wang <yingw787.personal@gmail.com>
2020-01-22 19:02:52 -0500

    Add partial ChapterExercises.hs.

---
+++ chp23/ChapterExercises.hs
@@ -0,0 +1,21 @@
+-- ChapterExercises.hs
+--
+-- (Unless otherwise stated, answers are from answer key:
+-- https://github.com/johnchandlerburnham/hpfp)
+module ChapterExercises where
+
+import WriteStateForYourself
+
+
+-- 1)
+-- (PERSONAL NOTE: I know it's something related to method 'id'...)
+get :: Moi s s
+get = Moi $ \s -> (s, s)
+
+-- 2)
+
+-- 3)
+
+-- 4)
+
+-- 5)
