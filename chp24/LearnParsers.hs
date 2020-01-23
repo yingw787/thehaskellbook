d17aedd1aabfb7bafc42aefdd40b9db3a7c6b294
Ying Wang <yingw787.personal@gmail.com>
2020-01-23 10:48:56 -0500

    Update LearnParsers.hs.

--- chp24/LearnParsers.hs
+++ chp24/LearnParsers.hs
@@ -7,3 +7,17 @@ import Text.Trifecta
 
 stop :: Parser a
 stop = unexpected "stop"
+
+-- read a single character '1'
+one = char '1'
+
+-- read a single character '1', then die
+one' = one >> stop
+-- equivalent to char '1' >> stop
+--
+-- use the sequencing operator in order to combine two parsers
+--
+-- (>>) :: Monad m => m a -> m b -> m b
+--
+-- The first return value is thrown away, but the monadic context from the first
+-- argument remains.
