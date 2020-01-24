d5096d438cdc8aa0e1effadf8f44f4a4bc2a2374
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 15:32:30 -0500

    Update EitherT.hs.

--- chp26/EitherT.hs
+++ chp26/EitherT.hs
@@ -1,4 +1,7 @@
 -- EitherT.hs
+--
+-- (Unless otherwise noted, answers from answer key:
+-- https://github.com/johnchandlerburnham/hpfp)
 module EitherT where
 
 
@@ -6,22 +9,31 @@ newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
 
 -- 1)
 instance Functor m => Functor (EitherT e m) where
-    fmap = undefined
+    fmap f (EitherT mea) = EitherT $ (fmap . fmap) f mea
 
 -- 2)
 instance Applicative m => Applicative (EitherT e m) where
-    pure = undefined
-    (<*>) f a = undefined
+    pure x = EitherT $ pure $ pure x
+
+    (<*>) (EitherT mefab) (EitherT mea) = EitherT $ (fmap (<*>)) mefab <*> mea
 
 -- 3)
 instance Monad m => Monad (EitherT e m) where
     return = pure
-    (>>=) v f = undefined
+    (>>=) (EitherT ema) f = EitherT $ do
+        v <- ema
+        case v of
+            Left e -> return (Left e)
+            Right a -> runEitherT (f a)
 
 -- 4)
+swapEither :: Either e a -> Either a e
+swapEither (Left e) = (Right e)
+swapEither (Right a) = (Left a)
+
 swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
-swapEitherT = undefined
+swapEitherT (EitherT ema) = EitherT $ swapEither <$> ema
 
 -- 5)
 eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
-eitherT = undefined
+eitherT fac fbc (EitherT mab) = (>>=) mab (either fac fbc)
