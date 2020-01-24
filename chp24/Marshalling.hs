e1d9f76ae7d6d18fca5634374fc7ea0dd2b25cd6
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 11:51:41 -0500

    Update Marshalling.hs.

--- chp24/Marshalling.hs
+++ chp24/Marshalling.hs
@@ -34,6 +34,31 @@ data Color =
     Yellow Annotation
     deriving (Eq, Show)
 
+instance FromJSON TestData where
+    parseJSON (Object v) =
+        TestData <$> v .: "section"
+                 <*> v .: "whatisit"
+    parseJSON _ =
+        fail "Expected an object for TestData"
+
+instance FromJSON Host where
+    parseJSON (Object v) = Host <$> v .: "host"
+    parseJSON _ =
+        fail "Expected an object for Host"
+
+instance FromJSON Color where
+    parseJSON (Object v) =
+        (Red <$> v .: "red") <|>
+        (Blue <$> v .: "blue") <|>
+        (Yellow <$> v .: "yellow")
+    parseJSON _ =
+        fail "Expected an object for Color"
+
+
+main = do
+    let d :: Maybe TestData
+        d = decode sectionJson
+    print d
 
 -- Error due to mixing Data.ByteString (ByteString) and Data.ByteString.Lazy
 -- (ByteString)
