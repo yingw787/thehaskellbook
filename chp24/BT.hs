e430589d33579efd08eff4e1d88c4ebc948cfdb7
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 11:26:35 -0500

    Update BT.hs.

--- chp24/BT.hs
+++ chp24/BT.hs
@@ -15,3 +15,32 @@ import Text.Parsec (Parsec, parseTest)
 trifP :: Show a => Parser a -> String -> IO ()
 trifP p i = do
     print $ parseString p mempty i
+
+parsecP :: Show a => Parsec String () a -> String -> IO ()
+parsecP = parseTest
+
+attoP :: Show a => A.Parser a -> ByteString -> IO ()
+attoP p i = print $ parseOnly p i
+
+nobackParse :: (Monad f, CharParsing f) => f Char
+nobackParse = (char '1' >> char '2' <|> char '3')
+
+-- Backtracks cursor if parsing fails
+tryParse :: (Monad f, CharParsing f) => f Char
+tryParse = try (char '1' >> char '2' <|> char '3')
+
+
+-- (PERSONAL NOTE: I'm not getting the errors shown in the book.)
+main :: IO ()
+main = do
+    -- trifecta
+    trifP nobackParse "13"
+    trifP tryParse "13"
+
+    -- parsec
+    parsecP nobackParse "13"
+    parsecP tryParse "13"
+
+    -- attoparsec
+    attoP nobackParse "13"
+    attoP tryParse "13"
