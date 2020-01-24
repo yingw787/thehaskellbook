05917b50b9e0b77742efde3d48fbecd6043c0e2b
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 10:34:05 -0500

    Update Ini.hs.

--- chp24/Ini.hs
+++ chp24/Ini.hs
@@ -139,3 +139,39 @@ main = hspec $ do
 
             print m
             shouldBe r' (Just (Header "blah"))
+
+    describe "Comment parsing" $
+        it "Skips comment before header" $ do
+            let p = skipComments >> parseHeader
+                i = "; woot\n[blah]"
+                m = parseByteString p mempty i
+                r' = maybeSuccess m
+
+            print m
+            shouldBe r' (Just (Header "blah"))
+
+    describe "Section parsing" $
+        it "can parse a simple section" $ do
+            let m = parseByteString parseSection mempty sectionEx
+                r' = maybeSuccess m
+                states = M.fromList [("Chris", "Texas")]
+                expected' = Just (Section (Header "states") states)
+
+            print m
+            shouldBe r' expected'
+
+    describe "INI parsing" $
+        it "Can parse multiple sections" $ do
+            let m = parseByteString parseIni mempty sectionEx''
+                r' = maybeSuccess m
+
+                sectionValues = M.fromList
+                                [ ("alias", "claw"),
+                                ("host", "wikipedia.org") ]
+
+                whatisitValues = M.fromList [("red", "intoothandclaw")]
+
+                expected' = Just (Config (M.fromList [ (Header "section", sectionValues), (Header "whatisit", whatisitValues)]))
+
+            print m
+            shouldBe r' expected'
