1dfe8bb4774cbab609b7f01bc329e3496f48c715
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 15:44:25 -0500

    Update ReaderT.hs.

--- chp26/ReaderT.hs
+++ chp26/ReaderT.hs
@@ -3,3 +3,18 @@ module ReaderT where
 
 
 newtype ReaderT r m a = ReaderT { runReaderT :: r -> m a }
+
+instance Functor m => Functor (ReaderT r m) where
+    fmap f (ReaderT rma) = ReaderT $ (fmap . fmap) f rma
+
+instance Applicative m => Applicative (ReaderT r m) where
+    pure a = ReaderT (pure (pure a))
+    (<*>) (ReaderT fmab) (ReaderT rma) = ReaderT $ (<*>) <$> fmab <*> rma
+
+instance Monad m => Monad (ReaderT r m) where
+    return = pure
+
+    (>>=) :: ReaderT r m a -> (a -> ReaderT r m b) -> RedaerT r m b
+    (>>=) (ReaderT rma) f = ReaderT $ \r -> do
+        a <- rma r
+        runReaderT (f a) r
