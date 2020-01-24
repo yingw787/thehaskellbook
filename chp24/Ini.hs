111f7290405321854ab44478ddb0bf5e3c368663
Ying Wang <yingw787.personal@gmail.com>
2020-01-24 09:59:36 -0500

    Update Ini.hs.

--- chp24/Ini.hs
+++ chp24/Ini.hs
@@ -12,7 +12,42 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text.IO as TIO
+-- stack install hspec
 import Test.Hspec
 
 import Text.RawString.QQ
 import Text.Trifecta
+
+
+headerEx :: ByteString
+headerEx = "[blah]"
+
+-- "[blah]" -> Section "blah"
+newtype Header = Header String deriving (Eq, Ord, Show)
+
+parseBracketPair :: Parser a -> Parser a
+parseBracketPair p =
+    char '[' *> p <* char ']'
+
+parseHeader :: Parser Header
+parseHeader =
+    parseBracketPair (Header <$> some letter)
+
+assignmentEx :: ByteString
+assignmentEx = "woot=1"
+
+type Name = String
+type Value = String
+type Assignments = Map Name Value
+
+parseAssignment :: Parser (Name, Value)
+parseAssignment = do
+    name <- some letter
+    _ <- char '='
+    val <- some (noneOf "\n")
+    skipEOL -- important!
+    return (name, val)
+
+-- Skip end of line and whitespace beyond.
+skipEOL :: Parser ()
+skipEOL = skipMany (oneOf "\n")
