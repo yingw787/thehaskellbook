0e47fbfb26d3598dac16a396861ca8ca5ededfdb
Ying Wang <yingw787.personal@gmail.com>
2020-01-26 14:03:51 -0500

    Update StateT.hs.

--- chp26/StateT.hs
+++ chp26/StateT.hs
@@ -1,5 +1,18 @@
 -- StateT.hs
+--
+-- (UNLESS OTHERWISE NOTED, ANSWERS FROM ANSWER KEY: )
 module StateT where
 
 
 newtype StateT s m a = State { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+    fmap f m = undefined
+
+instance Monad m => Applicative (StateT s m) where
+    pure = undefined
+    (<*>) = undefined
+
+instance Monad m => Monad (StateT s m) where
+    return = pure
+    (>>=) sma f = undefined
