18fe198e1e5986c41d68a920b950e98986d851da
Ying Wang <yingw787.personal@gmail.com>
2020-01-26 14:07:29 -0500

    Update StateT.hs.

--- chp26/StateT.hs
+++ chp26/StateT.hs
@@ -3,16 +3,24 @@
 -- (UNLESS OTHERWISE NOTED, ANSWERS FROM ANSWER KEY: )
 module StateT where
 
+import Control.Monad.Trans
 
-newtype StateT s m a = State { runStateT :: s -> m (a, s) }
+
+newtype StateT s m a = StateT { runStateT :: s -> m (a, s) }
 
 instance Functor m => Functor (StateT s m) where
-    fmap f m = undefined
+    fmap f (StateT sma) = StateT $ \s0 -> fstMap f <$> (sma s0) where
+        fstMap f (x, y) = (f x, y)
 
 instance Monad m => Applicative (StateT s m) where
-    pure = undefined
-    (<*>) = undefined
+    pure x = StateT $ \s0 -> pure (x, s0)
+    (<*>) (StateT smf) (StateT sma) = StateT $ \s0 -> do
+        (a, s1) <- sma s0
+        (f, s2) <- smf s1
+        return (f a, s2)
 
 instance Monad m => Monad (StateT s m) where
     return = pure
-    (>>=) sma f = undefined
+    (>>=) (StateT sma) f = StateT $ \s0 -> do
+        (a, s1) <- sma s0
+        runStateT (f a) s1
