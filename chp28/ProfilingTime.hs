2cceb0d378d1b26a4e4a631cd27905fa021e5f23
Ying Wang <yingw787.personal@gmail.com>
2020-01-28 17:48:54 -0500

    Add ProfilingTime.hs.

---
+++ chp28/ProfilingTime.hs
@@ -0,0 +1,18 @@
+-- ProfilingTime.hs
+module ProfilingTime where
+
+
+f :: IO ()
+f = do
+    print ([1..] !! 999999)
+    putStrLn "f"
+
+g :: IO ()
+g = do
+    print ([1..] !! 9999999)
+    putStrLn "g"
+
+main :: IO ()
+main = do
+    f
+    g
