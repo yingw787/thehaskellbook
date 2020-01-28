4c49da470d809836e74fb9e2644325c6839fc0b8
Ying Wang <yingw787.personal@gmail.com>
2020-01-28 18:27:11 -0500

    Update ApplicativeForms.hs.

--- chp28/ApplicativeForms.hs
+++ chp28/ApplicativeForms.hs
@@ -3,3 +3,17 @@
 -- (Constant applicative forms, or CAFs, are expressions with no free variables
 -- and held entirely in memory)
 module ApplicativeForms where
+
+
+incdInts :: [Integer]
+incdInts = map (+1) [1..]
+
+
+main :: IO ()
+main = do
+    print (incdInts !! 1000)
+    print (incdInts !! 9001)
+    print (incdInts !! 90010)
+    print (incdInts !! 9001000)
+    print (incdInts !! 9501000)
+    print (incdInts !! 9901000)
