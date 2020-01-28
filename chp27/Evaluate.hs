f609847e90a4a7b2edd9a71ff5e7a458946e06e8
Ying Wang <yingw787.personal@gmail.com>
2020-01-28 12:58:33 -0500

    Update Evaluate.hs.

--- chp27/Evaluate.hs
+++ chp27/Evaluate.hs
@@ -3,22 +3,36 @@ module Evaluate where
 
 
 -- 1)
+--
+-- evaluates to 1
 const 1 undefined
 
 -- 2)
+--
+-- evaluates to bottom
 const undefined 1
 
 -- 3)
+--
+-- evaluates to 1
 flip const undefined 1
 
 -- 4)
+--
+-- evaluates to bottom
 flip const 1 undefined
 
 -- 5)
+--
+-- evaluates to bottom
 const undefined undefined
 
 -- 6)
+--
+-- evaluates to 'a'
 foldr const 'z' ['a'..'e']
 
 -- 7)
+--
+-- evaluates to 'z'
 foldr (flip const) 'z' ['a'..'e']
