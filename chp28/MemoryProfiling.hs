d927a57a8288430d935f5ff530eb31e4b15934b9
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 15:16:15 -0500

    Add MemoryProfiling.hs.

---
+++ chp28/MemoryProfiling.hs
@@ -0,0 +1,19 @@
+-- MemoryProfiling.hs
+module MemoryProfiling where
+
+import Control.Monad
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified System.IO as SIO
+
+dictWords :: IO String
+dictWords = SIO.readFile "/usr/share/dict/words"
+
+dictWordsT :: IO T.Text
+dictWordsT = TIO.readFile "/usr/share/dict/words"
+
+
+main :: IO ()
+main = do
+    replicateM_ 1000 (dictWords >>= print)
+    replicateM_ 1000 (dictWordsT >>= TIO.putStrLn)
