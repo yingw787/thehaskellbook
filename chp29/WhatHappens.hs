a593bb4aa300080140c6004c6a9aa7f2cfaf618e
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 16:38:15 -0500

    Add WhatHappens.hs.

---
+++ chp29/WhatHappens.hs
@@ -0,0 +1,17 @@
+-- WhatHappens.hs
+module WhatHappens where
+
+import Control.Concurrent
+
+
+myData :: IO (MVar Int)
+myData = newEmptyMVar
+
+
+main :: IO ()
+main = do
+    mv <- myData
+    putMVar mv 0
+    mv' <- myData
+    zero <- takeMVar mv'
+    print zero
