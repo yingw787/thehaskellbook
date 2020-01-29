0acb0ccf0b9d34b9ac7b9d1aac6238c9a1f142f1
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 15:17:28 -0500

    Update MemoryProfiling.hs.

--- chp28/MemoryProfiling.hs
+++ chp28/MemoryProfiling.hs
@@ -4,6 +4,7 @@ module MemoryProfiling where
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy.IO as TLIO
 import qualified System.IO as SIO
 
 dictWords :: IO String
@@ -12,8 +13,12 @@ dictWords = SIO.readFile "/usr/share/dict/words"
 dictWordsT :: IO T.Text
 dictWordsT = TIO.readFile "/usr/share/dict/words"
 
+dictWordsTL :: IO TL.Text
+dictWordsTL = TLIO.readFile "/usr/share/dict/words"
+
 
 main :: IO ()
 main = do
     replicateM_ 1000 (dictWords >>= print)
     replicateM_ 1000 (dictWordsT >>= TIO.putStrLn)
+    replicateM_ 1000 (dictWordsTL >>= TLIO.putStrLn)
