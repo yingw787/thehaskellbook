916e4047bf6173e1f90bf7aee8f42f6da7ef0b30
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 14:30:10 -0500

    Add SlicingBench.hs

---
+++ chp28/SlicingBench.hs
@@ -0,0 +1,24 @@
+-- SlicingBench.hs
+module SlicingBench where
+
+import Criterion.Main
+import qualified Data.Vector as V
+
+
+slice :: Int -> Int -> [a] -> [a]
+slice from len xs = take len (drop from xs)
+
+l :: [Int]
+l = [1..1000]
+
+v :: V.Vector Int
+v = V.fromList [1..1000]
+
+
+main :: IO ()
+main = defaultMain
+    [   bench "slicing list" $
+        whnf (head . slice 100 900) l
+    ,   bench "slicing vector" $
+        whnf (V.head . V.slice 100 900) v
+    ]
