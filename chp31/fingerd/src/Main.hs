886efe14489f719b188636d1a6d038ad7d03c89e
Ying Wang <yingw787.personal@gmail.com>
2020-01-30 10:30:26 -0500

    Update src/Main.hs.

--- chp31/fingerd/src/Main.hs
+++ chp31/fingerd/src/Main.hs
@@ -1,5 +1,26 @@
+-- Main.hs
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import Control.Exception
+import Control.Monad (forever)
+import Data.List (intersperse)
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+
+import Data.Typeable
+import Database.SQLite.Simple hiding (close)
+import qualified Database.SQLite.Simple as SQLite
+
+import Database.SQLite.Simple.Types
+import Network.Socket hiding (close, recv)
+import Data.ByteString (ByteString)
+
+import qualified Data.ByteString as BS
+import Network.Socket.ByteSTring (recv, sendAll)
+import Text.RawString.QQ
