08186d9a7f68f887d504c17e96fc00d1420234b8
Ying Wang <yingw787.personal@gmail.com>
2020-01-30 10:45:34 -0500

    Update src/Main.hs complete book.

--- chp31/fingerd/src/Main.hs
+++ chp31/fingerd/src/Main.hs
@@ -96,6 +96,67 @@ createDatabase = do
   where meRow :: UserRow
         meRow = (Null, "yingw787", "/bin/bash", "/home/yingw787", "Ying Wang", "123-456-6789")
 
+returnUsers :: Connection -> Socket -> IO ()
+returnUsers dbConn soc = do
+  rows <- query_ dbConn allUsers
+
+  let usernames = map username rows
+      newlineSeparated = T.concat $ intersperse "\n" usernames
+
+  sendAll soc (encodeUtf8 newlineSeparated)
+
+formatUser :: User -> ByteString
+formatUser (User _ username shell homeDir realName _) = BS.concat
+  [ "Login: ", e username, "\t\t\t\t",
+    "Name: ", e realName, "\n",
+    "Directory: ", e homeDir, "\t\t\t",
+    "Shell: ", e shell, "\n" ]
+  where e = encodeUtf8
+
+returnUser :: Connection -> Socket -> Text -> IO ()
+returnUser dbConn soc username = do
+  maybeUser <- getUser dbConn (T.strip username)
+
+  case maybeUser of
+    Nothing -> do
+      putStrLn ("Couldn't find matching user\
+                \ for username: "
+                ++ (show username))
+      return ()
+
+    Just user -> sendAll soc (formatUser user)
+
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name -> returnUser dbConn soc (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries dbConn sock = forever $ do
+  (soc, _) <- accept sock
+  putStrLn "Got connection, handling query"
+
+  handleQuery dbConn soc
+  sClose soc
+
 
 main :: IO ()
-main = createDatabase
+main = withSocketsDo $ do
+  addrinfos <- getAddrInfo
+                (Just (defaultHints { addrFlags = [AI_PASSIVE] } ))
+                Nothing (Just "79")
+
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  bindSocket sock (addrAddress serveraddr)
+
+  listen sock 1
+
+  conn <- open "finger.db"
+  handleQueries conn sock
+
+  SQLite.close conn
+  sClose sock
