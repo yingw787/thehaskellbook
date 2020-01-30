05df72056bf097332069d9adfcafca6066293f79
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 23:01:47 -0500

    Add partial AsyncExceptions.hs.

---
+++ chp30/AsyncExceptions.hs
@@ -0,0 +1,26 @@
+-- AsyncExceptions.hs
+module AsyncExceptions where
+
+import Control.Concurrent (forkIO, threadDelay)
+
+import Control.Exception
+import System.IO
+
+
+openAndWrite :: IO ()
+openAndWrite = do
+    h <- openFile "test.dat" WriteMode
+    threadDelay 1500
+
+    hPutStr h (replicate 100000000 '0' ++ "abc")
+    hClose h
+
+data PleaseDie = PleaseDie deriving Show
+instance Exception PleaseDie
+
+
+main :: IO ()
+main = do
+    threadId <- forkIO openAndWrite
+    threadDelay 1000
+    throwTo threadId PleaseDie
