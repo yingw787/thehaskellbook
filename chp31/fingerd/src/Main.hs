c6fecab2ea99029985813928c298cc497239e0c5
Ying Wang <yingw787.personal@gmail.com>
2020-01-30 10:30:32 -0500

    Update src/Main.hs.

--- chp31/fingerd/src/Main.hs
+++ chp31/fingerd/src/Main.hs
@@ -22,5 +22,49 @@ import Network.Socket hiding (close, recv)
 import Data.ByteString (ByteString)
 
 import qualified Data.ByteString as BS
-import Network.Socket.ByteSTring (recv, sendAll)
+import Network.Socket.ByteString (recv, sendAll)
 import Text.RawString.QQ
+
+
+data User =
+  User {
+    userId :: Integer
+  , username :: Text
+  , shell :: Text
+
+  , homeDirectory :: Text
+  , realName :: Text
+  , phone :: Text
+  } deriving (Eq, Show)
+
+instance FromRow User where
+  fromRow = User  <$> field
+                  <*> field
+                  <*> field
+                  <*> field
+                  <*> field
+                  <*> field
+
+instance ToRow User where
+  toRow (User id_ username shell homeDir realName phone) = toRow (id_, username, shell, homeDir, realName, phone)
+
+createUsers :: Query
+createUsers = [r|
+CREATE TABLE IF NOT EXISTS users
+  ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    shell TEXT, homeDirectory TEXT,
+    realName TEXT, phone TEXT
+  )
+|]
+
+insertUser :: Query
+insertUser =
+  "INSERT INTO users\
+  \ VALUES (?, ?, ?, ?, ?, ?)"
+
+allUsers :: Query
+allUsers = "SELECT * from users"
+
+getUserQuery :: Query
+getUserQuery = "SELECT * from users where username = ?"
