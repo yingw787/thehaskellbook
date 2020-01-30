107e325a468725f28cff4f6d0a4077de1963e607
Ying Wang <yingw787.personal@gmail.com>
2020-01-29 22:45:18 -0500

    Update OurExceptions.hs.

--- chp30/OurExceptions.hs
+++ chp30/OurExceptions.hs
@@ -4,14 +4,14 @@ module OurExceptions where
 import Control.Exception
 
 
-data NotDivThree = NotDivThree deriving (Eq, Show)
+data NotDivThree = NotDivThree Int deriving (Eq, Show)
 instance Exception NotDivThree
 
-data NotEven = NotEven deriving (Eq, Show)
+data NotEven = NotEven Int deriving (Eq, Show)
 instance Exception NotEven
 
 evenAndThreeDiv :: Int -> IO Int
 evenAndThreeDiv i
-    | rem i 3 /= 0 = throwIO NotDivThree
-    | odd i = throwIO NotEven
+    | rem i 3 /= 0 = throwIO (NotDivThree i)
+    | odd i = throwIO (NotEven i)
     | otherwise = return i
